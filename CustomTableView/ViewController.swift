9d131754b1f1ca370d32c2b73799cf9ddd41e08a
Drew <wxl33lv51@vossie.net>
2023-05-22 16:09:48 +0200

    Initial Commit

---
+++ CustomTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomTableView
+//
+//  Created by DA MAC M1 155 on 2023/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
